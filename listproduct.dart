e0c3a35afc9730c93b4383d5fbef56ff128bdb66
tram22ngoc <121414631+tram22ngoc@users.noreply.github.com>
2023-06-10 17:23:27 +0700

    nút nhấn trang sản phẩm

---
+++ listproduct.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ListProduct extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.notification_add),
+            color: Colors.black,
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
